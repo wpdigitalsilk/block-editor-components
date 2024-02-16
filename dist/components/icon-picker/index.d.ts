f65c07285cf5832ccf0476fea9da9a5770447e6c
Stefan Vasiljevic <stefan@digitalsilk.com>
2024-02-16 13:17:43 +0100

    update

---
+++ dist/components/icon-picker/index.d.ts
@@ -0,0 +1,19 @@
+export function IconPicker(props: any): import("react").JSX.Element;
+export namespace IconPicker {
+    namespace defaultProps {
+        let width: number;
+        let height: number;
+        let icon: {};
+    }
+    namespace propTypes {
+        let width_1: PropTypes.Requireable<number>;
+        export { width_1 as width };
+        let height_1: PropTypes.Requireable<number>;
+        export { height_1 as height };
+        export let onSelectIcon: PropTypes.Validator<(...args: any[]) => any>;
+        let icon_1: PropTypes.Requireable<object>;
+        export { icon_1 as icon };
+    }
+}
+import PropTypes from 'prop-types';
+//# sourceMappingURL=index.d.ts.map
